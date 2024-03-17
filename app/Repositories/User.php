2f03b75b79a68789d47feb55b861ec7764e85f6f
Ely Edison Matos <ely.matos@gmail.com>
2024-03-16 21:29:53 -0300

    test

--- app/Repositories/User.php
+++ app/Repositories/User.php
@@ -86,6 +86,7 @@ class User extends Repository
             //$this->setData($userData);
             $this->registerLogin();
             //$this->groups = [$group];
+            // ffff
             $this->saveAssociation('groups');
             PersistenceManager::commit();
         } catch (\Exception $e) {
