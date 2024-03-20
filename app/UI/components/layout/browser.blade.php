d647b32b76960d68a098450b6fdf6f4c152e1f0b
Ely Edison Matos <ely.matos@gmail.com>
2024-03-19 23:24:58 -0300

    responsive

--- app/UI/components/layout/browser.blade.php
+++ app/UI/components/layout/browser.blade.php
@@ -1,7 +1,25 @@
-<div class="nav">
-    {{$nav}}
-</div>
-<div class="main">
-    {{$main}}
-</div>
+<x-layout.index>
+    <header class="main-header">
+        <div class="hxRow hxGutterless">
+            <div class="hxCol hxSpan-8">
+                <h1>
+                    {{$title}}
+                </h1>
+            </div>
+            <div class="hxCol hxSpan-4 actions">
+                {{$actions}}
+            </div>
+        </div>
+    </header>
+    <div class="hxRow">
+        <div class="hxCol hxSpan-11-xs hxSpan-11-sm hxSpan-10-md hxSpan-9-lg hxSpan-6-xl mx-auto">
+            <div class="search">
+                {{$search}}
+            </div>
+            <div class="grid">
+                {{$grid}}
+            </div>
+        </div>
+    </div>
+</x-layout.index>
 
--- app/UI/views/Structure/Frame/browse.blade.php
+++ app/UI/views/Structure/Frame/browse.blade.php
@@ -1,45 +1,48 @@
-@extends('Structure.Frame.main')
-@section('content')
-    <x-layout.browser>
-        <x-slot:nav>
-            <x-form-search id="frameSearch">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
-                <x-search-field
-                    id="search_frame"
-                    value="{{$search->frame}}"
-                    placeholder="Search Frame"
-                ></x-search-field>
-                <x-search-field
-                    id="search_fe"
-                    value="{{$search->fe}}"
-                    placeholder="Search FE"
-                ></x-search-field>
-                <x-search-field
-                    id="search_lu"
-                    value="{{$search->lu}}"
-                    placeholder="Search LU"
-                ></x-search-field>
-{{--                <x-combobox.framal-domain--}}
-{{--                    id="search_idFramalDomain"--}}
-{{--                    placeholder="Domain"--}}
-{{--                    value="{{$search->idFramalDomain}}"--}}
-{{--                ></x-combobox.framal-domain>--}}
-{{--                <x-combobox.framal-type--}}
-{{--                    id="search_idFramalType"--}}
-{{--                    placeholder="Type"--}}
-{{--                    value="{{$search->idFramalType}}"--}}
-{{--                ></x-combobox.framal-type>--}}
-                <x-submit
-                    label="Search"
-                    hx-post="/frame/grid"
-                    hx-target="#frameGrid"
-                ></x-submit>
-            </x-form-search>
-        </x-slot:nav>
-        <x-slot:main>
-            <div id="frameGrid" class="mainGrid">
-                @include('Structure.Frame.grid')
-            </div>
-        </x-slot:main>
-    </x-layout.browser>
-@endsection
+<x-layout.browser>
+    <x-slot:title>
+        Frames
+    </x-slot:title>
+    <x-slot:actions>
+        <x-button label="New" color="secondary" href="/frame/new"></x-button>
+    </x-slot:actions>
+    <x-slot:search>
+        <x-form-search id="frameSearch">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
+            <x-search-field
+                id="search_frame"
+                value="{{$search->frame}}"
+                placeholder="Search Frame"
+            ></x-search-field>
+            <x-search-field
+                id="search_fe"
+                value="{{$search->fe}}"
+                placeholder="Search FE"
+            ></x-search-field>
+            <x-search-field
+                id="search_lu"
+                value="{{$search->lu}}"
+                placeholder="Search LU"
+            ></x-search-field>
+            {{--                <x-combobox.framal-domain--}}
+            {{--                    id="search_idFramalDomain"--}}
+            {{--                    placeholder="Domain"--}}
+            {{--                    value="{{$search->idFramalDomain}}"--}}
+            {{--                ></x-combobox.framal-domain>--}}
+            {{--                <x-combobox.framal-type--}}
+            {{--                    id="search_idFramalType"--}}
+            {{--                    placeholder="Type"--}}
+            {{--                    value="{{$search->idFramalType}}"--}}
+            {{--                ></x-combobox.framal-type>--}}
+            <x-submit
+                label="Search"
+                hx-post="/frame/grid"
+                hx-target="#frameGrid"
+            ></x-submit>
+        </x-form-search>
+    </x-slot:search>
+    <x-slot:grid>
+        <div id="frameGrid">
+            @include('Structure.Frame.grid')
+        </div>
+    </x-slot:grid>
+</x-layout.browser>
